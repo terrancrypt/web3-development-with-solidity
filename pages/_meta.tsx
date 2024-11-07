fa8cf7bef2757a42736ccb46aeb016e1917ddaef
Tường Thái <terrrancrypt@Tuongs-MacBook-Air.local>
2024-11-07 13:48:35 +0700

    init

---
+++ pages/_meta.tsx
@@ -0,0 +1,10 @@
+export default {
+  index: 'Welcome',
+  blockchain_co_ban: '#1: Blockchain Cơ bản',
+  solidity_co_ban: '#2: Ngôn Ngữ Solidity',
+  smart_contract_factory: '#3: Smart Contract Factory',
+  decentralized_crowdfunding_contract: '#4: Decentralized Crowfunding Contract',
+  foundry_framework: '#5: Foundry Framework',
+  smart_contract_testing: '#6: Smart Contract Testing',
+  web3_front_end: '#7: Web3 Front-end',
+};
---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+  logo: <span>Web3 Development with Solidity</span>,
+  project: {
+    link: "https://github.com/terrancrypt/web3-development-with-solidity",
+  },
+  docsRepositoryBase:
+    "https://github.com/terrancrypt/web3-development-with-solidity",
+  feedback: {
+    content: "Có câu hỏi? Hãy để lại feedback →",
+    title: "Feedback cho “Web3 Development with Solidity”",
+    useLink: () =>
+      `https://github.com/terrancrypt/web3-development-with-solidity/discussions/new?category=general&title=Feed back for "Web3 Development with Solidity"`,
+  },
+};
