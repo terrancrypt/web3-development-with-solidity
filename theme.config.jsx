3c6d225ca8117a4a949ec4e4c1e4202ade21a3b8
terrancrypt <thaiphamngoctuong@gmail.com>
2024-11-11 14:55:28 +0700

    change style

--- theme.config.jsx
+++ theme.config.jsx
@@ -13,4 +13,31 @@ export default {
   editLink: {
     component: null,
   },
+  toc: {
+    component: false,
+  },
+  head() {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      'https://web3.terrancrypt.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || 'Web3 Development with Solidity'}
+        />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            'The ultimate guide to Web3 development using Solidity'
+          }
+        />
+      </>
+    );
+  },
 };
