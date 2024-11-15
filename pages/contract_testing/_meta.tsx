9d39c51f551cb4c0363e6f329970f3ea65cd52d4
terrancrypt <thaiphamngoctuong@gmail.com>
2024-11-15 08:40:28 +0700

    finish section 6

--- pages/contract_testing/_meta.tsx
+++ pages/contract_testing/_meta.tsx
@@ -1,11 +1,8 @@
 export default {
   index: 'Setup dự án',
   install_chainlink: 'Install Chainlink Library',
-  remappings: 'Remappings',
   why_testing: 'Tại sao phải Testing',
   setup_test_file_and_test_case: 'Thiết lập Test File & Test Case',
-  setup_test_for_crowdfunding_contract:
-    'Thiết lập Test cho Crowdfunding Contract',
   unit_testing_for_function_fund: 'Unit Testing cho Function `fund()`',
   use_mock_contract_for_local_testing:
     'Sử dụng Mock Contract cho Local Testing',
@@ -14,9 +11,8 @@ export default {
     'Thiết lập Deploy & Helper Config Script',
   use_script_for_test_file: 'Sử dụng Script cho Test File',
   testing_coverage: 'Testing Coverage',
-  install_openzeppelin_library: 'Install OpenZeppelin Library',
+  install_openzeppelin: 'Install OpenZeppelin Library',
   unit_testing_for_function_withdraw: 'Unit Testing cho Function `withdraw()`',
-  check_test_coverage: 'Kiểm tra Độ Bao Phủ Test',
   interactions_script: 'Interactions Script',
   integration_test: 'Integration Test',
   upgrade_helper_config_script: 'Nâng cấp Helper Config Script',
